df9a6b01fb85502d14398bf82044d52f53666a4f
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-07-17 22:11:37 +0500

    commit

--- firebasconfig.js
+++ firebasconfig.js
@@ -1,18 +1,37 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getFirestore,collection, doc, setDoc, addDoc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
-import { getAuth, signOut , createUserWithEmailAndPassword, signInWithEmailAndPassword , onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, addDoc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import { getAuth, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM60KWvtkG839mU292mJzblIqWalqkkJo",
-  authDomain: "social-media-app-7593f.firebaseapp.com",
-  projectId: "social-media-app-7593f",
-  storageBucket: "social-media-app-7593f.appspot.com",
-  messagingSenderId: "57654938598",
-  appId: "1:57654938598:web:616b84298b629aa89d2427"
-};
+    apiKey: "AIzaSyAM60KWvtkG839mU292mJzblIqWalqkkJo",
+    authDomain: "social-media-app-7593f.firebaseapp.com",
+    projectId: "social-media-app-7593f",
+    storageBucket: "social-media-app-7593f.appspot.com",
+    messagingSenderId: "57654938598",
+    appId: "1:57654938598:web:616b84298b629aa89d2427"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export {app, db, collection, addDoc ,doc, setDoc, getDocs, getFirestore , auth, createUserWithEmailAndPassword , signInWithEmailAndPassword , onAuthStateChanged , signOut , getDoc};
+export {
+  app,
+  db,
+  collection,
+  addDoc,
+  doc,
+  setDoc,
+  getDocs,
+  getFirestore,
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  getDoc,
+  getStorage,
+  ref,
+  getDownloadURL
+};
