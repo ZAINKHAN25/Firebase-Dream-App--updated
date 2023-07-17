c512ef965371c84edb28843dbe8250b8afb5a2a2
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-07-17 22:36:18 +0500

    Bilal Zafar kam nahi hoa

--- firebasconfig.js
+++ firebasconfig.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
 export {
   app,
@@ -33,5 +34,6 @@ export {
   getDoc,
   getStorage,
   ref,
-  getDownloadURL
+  getDownloadURL,
+  storage
 };
